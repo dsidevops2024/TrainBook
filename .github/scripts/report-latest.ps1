3aec151e794f5026d87aaf10bd0c0cd90ae5c872
mspriya92 <157378385+mspriya92@users.noreply.github.com>
2025-02-18 22:21:09 +0530

    Update report-latest.ps1

--- .github/scripts/report-latest.ps1
+++ .github/scripts/report-latest.ps1
@@ -35,7 +35,7 @@ $controllers = @("check-component-input", "create-environment-matrix", "set-envi
     $componentStatus = ($controllerStatus -split "$component status: ")[1] -split ", " | Select-Object -First 1
     #Add-Content -Path $env:GITHUB_OUTPUT -Value "$component-status=$component status: $componentStatus"
      # Set each component status as output
-    echo "$component-status=$component status: $componentStatus" >> $GITHUB_OUTPUT
+    echo "$component-status=$component_status: $componentStatus" >> $GITHUB_OUTPUT
 }
 
 # Set phaseStatus as output
