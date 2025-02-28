653aa77ccf2fca8402015ef9a20e773bea87fcce
mspriya92 <157378385+mspriya92@users.noreply.github.com>
2025-02-28 17:33:58 +0530

    Update Newcheck.ps1

--- .github/scripts/Newcheck.ps1
+++ .github/scripts/Newcheck.ps1
@@ -4,7 +4,7 @@
 param (
     [string]$controllerStatus,
     [string]$phaseStatus,
-    [string[]]$componentStatus  # Receives component statuses as an array
+    [string]$componentStatus  # Receives component statuses as an array
 )
 Write-Output "Controller Status: $controllerStatus"
 Write-Output "Phase Status: $phaseStatus"
